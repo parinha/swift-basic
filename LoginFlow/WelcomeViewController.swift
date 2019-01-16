649a479dc2197b47b5ca59f534f9dd0559d60f5e
parinha <orsereiparinha@gmail.com>
2019-01-16 10:45:36 +0700

    UI: welcome screen

---
+++ LoginFlow/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginFlow
+//
+//  Created by Parinha on 1/16/19.
+//  Copyright © 2019 Parinha. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
