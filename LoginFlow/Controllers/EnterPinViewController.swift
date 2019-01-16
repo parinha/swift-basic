8c6c76cebd0dca428aa306bd4ad8a5f781a19cdc
parinha <orsereiparinha@gmail.com>
2019-01-16 17:13:27 +0700

    dismiss keyboard

--- LoginFlow/Controllers/EnterPinViewController.swift
+++ LoginFlow/Controllers/EnterPinViewController.swift
@@ -12,19 +12,6 @@ class EnterPinViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: Selector("endEditing:")))
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
